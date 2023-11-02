270e8a0e739609e4605fecc0229726d3104a603a
James Bate <james.bate@uibk.ac.at>
2023-11-02 23:23:39 +0100

    added tomography plot

--- stdlib_sensing/__init__.py
+++ stdlib_sensing/__init__.py
@@ -1,3 +1,4 @@
 from stdlib_sensing.enumerators import setting_enumerator, power_enumerator
 from stdlib_sensing.projection_analysis import ProjectionAnalysis
 from stdlib_sensing.plot_template import PlotTemplate
+from stdlib_sensing.matrix_histogram import matrix_histogram
