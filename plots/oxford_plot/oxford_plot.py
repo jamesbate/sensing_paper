6457c5cc3d0fee11dbc7720d5a6a7b8971479d4e
Arne Hamann <git@arne.email>
2023-11-08 17:27:10 +0100

    Added third example, introduced a custom base signal (instead of nostark) to detect changes on.
    Signed-off-by: Arne Hamann <git@arne.email>

--- plots/oxford_plot/oxford_plot.py
+++ plots/oxford_plot/oxford_plot.py
@@ -14,15 +14,20 @@ observable = qt.tensor([qt.sigmaz()]*3)
 
 ##=======##
 #Example 1
-# signals = ['-25.5db']#'-25.5db'
-# groups = 3
-# point_index = 19 #Sit at phase 0.51525424
+#signals = ['nosignal','-25.5db']#'-25.5db'
+#groups = 3
+#point_index = 19 #Sit at phase 0.51525424
 
 ##=======##
 #Example 2
-signals = ['-27.5db']
+# signals = ['nosignal', '-27.5db']
+# groups = 3
+# point_index = 19 #Sit at phase 0.51525424
+
+#Example 3
+signals = ['-24db', '-24.5db']
 groups = 3
-point_index = 19 #Sit at phase 0.51525424
+point_index = 29 #Sit at phase 0.786441
 
 ##------------------READ PARITY--------------##
 
@@ -50,25 +55,20 @@ for power in power_enumerator.keys():
 
 ##------------------PLOT SIGNAL--------------##
 
-pt = PlotTemplate((1, 2))
+pt = PlotTemplate((1, len(signals)))
 ax, fig = pt.generate_figure(fig_ret=True)
 
-for n, power in enumerate(power_enumerator.keys()):
+for n, power in enumerate(signals):
     for m, setting in enumerate(['entangledsensing', 'productsensing']):#setting_enumerator.keys(), ['entangledsensing']
-        if power not in signals and power != 'nosignal':
-            continue
-        if n == 0:
-            _ax = ax[0]
-        else:
-            _ax = ax[1]
+        _ax = ax[n]
         pt.plot(range(groups), parity[:, power_enumerator[power], setting_enumerator[setting], point_index], _ax, errors = dparity[:, power_enumerator[power], setting_enumerator[setting], point_index], colour_index = m, plottype = 'scatter')
         _ax.axhline(np.mean(parity[:, power_enumerator[power], setting_enumerator[setting], point_index]), color = pt.colours[m], label = setting)
         _ax.set_ylim((-0.75, 0.75))
         _ax.legend()
         _ax.set_xticks(range(groups))
 
-ax[0].set_title('no signal')
-ax[1].set_title(f'with signal {signals}')
+        _ax.set_title('no signal' if power == "nosignal" else f'with signal {power}')
+
 ax[0].set_ylabel('Parity signal')
 ax[0].set_xlabel('Measurement')
 fig.savefig('plots/oxford_plot/oxfordplotexample2.png')
